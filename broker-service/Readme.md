fcb9c612a9b11c55971045da481aa170e6143114
fxmbx <funbiolaore@gmail.com>
2022-07-17 20:43:49 +0100

    set up broker service and frontend communication

---
+++ broker-service/Readme.md
@@ -0,0 +1,14 @@
+### Broker-Service
+
+This service will be responsible for middleware connection
+communication from the front end to the backend
+
+## go-chi
+
+go-chi is a composable router for building http services
+
+## installaton
+
+go get github.com/go-chi/chi/v5
+go get github.com/go-chi/chi/v5/middleware
+go get github.com/go-chi/cors
---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s\n", port)
+
+	//define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	//start server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
